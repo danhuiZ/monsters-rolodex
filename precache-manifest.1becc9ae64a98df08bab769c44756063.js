23627a71cca9444e54b14f7fd9880d4a75c3c488
danhuiZ <danhuizzz@gmail.com>
2019-07-23 04:52:25 -0400

    Updates

---
+++ precache-manifest.1becc9ae64a98df08bab769c44756063.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249019d6338cd7779480960948a79231",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "54a286783a57894dd8ea",
+    "url": "/monsters-rolodex/static/css/main.4402da2a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "54a286783a57894dd8ea",
+    "url": "/monsters-rolodex/static/js/main.8a783af9.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
